e8dd1940ad5548a30dd2e067accc26e3e9c0ede0
chenken <chenkenck@outlook.com>
2019-03-20 11:13:34 +0800

    add help info in src/init.sh

--- src/init.sh
+++ src/init.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
+## -------------------------------------------------------------- ##
+## You should edit this file to make variables and aliases match
+## configurations on your device
+## -------------------------------------------------------------- ##
 
-## ANNOVAR
+# ANNOVAR
 export annovar="$HOME/Tools/annovar"
 export humandb="$HOME/Data/annovar/humandb"
 export annotate_variation="$annovar/annotate_variation.pl"
@@ -8,13 +12,17 @@ export convert2annovar="$annovar/convert2annovar.pl"
 export table_annovar="$annovar/table_annovar.pl"
 export hg19_ensGene="$HOME/Data/annovar/humandb/hg19_ensGene.txt"
 
-## samtools
+# python
+alias python3='/usr/bin/env python3'
+alias python2='/usr/bin/env python2'
+
+# samtools
 export samtools="/usr/bin/samtools"
 
-## LIBSVM
-export svm_scale="svm-scale"
-export svm_predict="svm-predict"
+# LIBSVM
 
-## reference genome
-export hg19_genome="$HOME/Data/reference/ucsc.hg19/ucsc.hg19.fasta"
+export svm_scale="/home/chenken/tmp/test_PredPSI-SVR_repo/libsvm-3.23/svm-scale"
+export svm_predict="/home/chenken/tmp/test_PredPSI-SVR_repo/libsvm-3.23/svm-predict"
 
+# reference genome
+export hg19_genome="/home/chenken/db/hg19/hg19.fa"
